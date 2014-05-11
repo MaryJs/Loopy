37f1fc56b3853d81e19e1d787a07a9f9b672120d
Natalia Tsybulenko <nata-tsyb@Natalias-MacBook-Pro.local>
2014-05-11 14:07:25 +0300

    add project

---
+++ Loopy/Loopy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Loopy
+//
+//  Created by Natalia Tsybulenko on 5/11/14.
+//  Copyright (c) 2014 Natalia Tsybulenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Loopy/Loopy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Loopy
+//
+//  Created by Natalia Tsybulenko on 5/11/14.
+//  Copyright (c) 2014 Natalia Tsybulenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
